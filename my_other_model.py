d75afd0526e50a4699ba74bae762189023533675
Jake Ferguson <j5fergus@UWATERLOO.CA>
2024-03-26 12:00:10 -0400

    Added a new model

---
+++ my_other_model.py
@@ -0,0 +1,6 @@
+def my_other_model(x):
+    return 1.7
+
+shoe_size = 13
+print("A person with shoe size %i is probably around %f metres tall"%(shoe_size, my_other_model(shoe_size)))
+print("I'm a silly model which just returns 1.7 whatever I recieve.")
